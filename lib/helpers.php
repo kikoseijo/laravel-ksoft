49da093da875c25917c9d808ccef6fa49a4f01ef
Kiko Seijo <kiko@sunnyface.com>
2019-04-20 22:18:31 +0200

    Added has_route method to helpers.. Added normalizeString method to helpers.

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,1 +1,34 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+if (!function_exists('route_has')) {
+    function route_has($route_name, $params = [], $absolute = true): string
+    {
+        if (str_contains($route_name, '@')) {
+            return action($route_name, $params);
+        }
+
+        if (!Route::has($route_name)) {
+            return url($route_name);
+        }
+
+        return route($route_name, $params, $absolute);
+    }
+}
+
+if (!function_exists('normalizeString')) {
+    function normalizeString($text, $limit = 0): string
+    {
+        if (!$text) {
+            return '';
+        }
+        $res = '';
+        if ($limit > 0) {
+            $res = str_limit($text, $limit);
+        } else {
+            $res = $text;
+        }
+        return ucfirst(mb_strtolower($res));
+    }
+}
