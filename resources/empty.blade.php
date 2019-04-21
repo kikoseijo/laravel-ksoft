e61f372290d1ad191f4958d6920de7cb537a18f4
Kiko Seijo <kiko@sunnyface.com>
2019-04-22 00:24:44 +0200

    Customized empty view with vars.

--- resources/empty.blade.php
+++ resources/empty.blade.php
@@ -1,7 +1,8 @@
-<!-- ksoft::empty -->
+<!-- ksoft::empty empty_icon, empty_text,  -->
 <div class="text-center my-5">
     <div class="mt-4 text-muted">
-        <i class="fal fa-folder-open fa-4x"></i>
+        <i class="fal fa-{{$empty_icon ?? 'folder-open'}} fa-4x"></i>
     </div>
-    <p class="h4 display-4">{{ __('No records found!') }}</p>
+    <p class="h4 display-4">{{ $empty_text ?? __('No records found!') }}</p>
+    {{ $slot ?? '' }}
 </div>
