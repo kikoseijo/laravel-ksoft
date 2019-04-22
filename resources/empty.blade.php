d8ac643c128205feff7d1d7dd4023eb7a4d9e043
Kiko Seijo <kiko@sunnyface.com>
2019-04-22 19:13:42 +0200

    Smaller empty view text.

--- resources/empty.blade.php
+++ resources/empty.blade.php
@@ -1,8 +1,8 @@
 <!-- ksoft::empty empty_icon, empty_text,  -->
 <div class="text-center my-5">
-    <div class="mt-4 text-muted">
-        <i class="fal fa-{{$empty_icon ?? 'folder-open'}} fa-4x"></i>
+    <div class="mt-5 text-muted">
+        <i class="fal fa-4x fa-{{$empty_icon ?? 'folder-open'}}"></i>
     </div>
-    <p class="h4 display-4">{{ $empty_text ?? __('No records found!') }}</p>
+    <p class="h4 text-muted {{$empty_text_style ?? ''}}">{{ $empty_text ?? __('No records found!') }}</p>
     {{ $slot ?? '' }}
 </div>
