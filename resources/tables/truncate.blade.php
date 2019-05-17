a7a2ed75738063dc944391c1e48e8f6862122072
Kiko Seijo <kiko@sunnyface.com>
2019-05-17 12:26:29 +0200

    Added ksoft::tables.truncate blade component.

---
+++ resources/tables/truncate.blade.php
@@ -0,0 +1,6 @@
+<!-- ksoft::tables.truncate -->
+<td class="align-middle {{$td_class ?? ''}}">
+    <div class="excerpt text-truncate {{$div_class ?? ''}}" style="max-width:{{$width ?? '100'}}px;">
+        {!! $item_data ?? '' !!}
+    </div>
+</td>
